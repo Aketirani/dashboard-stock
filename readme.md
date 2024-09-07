cd3bafb996f46af80ba14fc325a3f1fc7ee9fa5c
Aria Ketirani <122474903+Aketirani@users.noreply.github.com>
2024-09-07 11:19:28 +0200

    readme and formatting updated

--- readme.md
+++ readme.md
@@ -1,147 +1,32 @@
-# Gender Recognition By Voice Analysis
-This project focuses on leveraging acoustic features extracted from voice recordings to predict the gender of the speaker. Utilizing a large dataset and the XGBoost machine learning library, the project aims to develop a robust model for gender classification.
+# Dashboard Stock
+
+This project is a web-based dashboard for visualizing stock market data, specifically focusing on the S&P 500 index. The dashboard provides real-time data, historical performance, and investment prediction features.
 
 ### Table of Contents
 - [Project Overview](#project-overview)
 - [Structure](#structure)
-- [Dataset](#dataset)
-- [Model Selection](#model-selection)
-- [Model Architecture](#model-architecture)
-- [Model Features](#model-features)
-- [Model Performance](#model-performance)
-- [PostgreSQL Integration](#postgresql-integration)
-- [Conclusion](#conclusion)
+- [Features](#features)
 - [Requirements](#requirements)
 - [Execution](#execution)
-- [Unit Tests](#unit-tests)
 - [Developer](#developer)
 
 ### Project Overview
-Voice recordings can be analyzed to infer a wide range of details including the content spoken, the emotional state, gender, and even the identity of the speaker. This project centers on gender recognition, aiming to create a model capable of identifying gender-specific patterns and features in voice recordings.
+The Dashboard Stock project aims to provide users with a comprehensive tool for monitoring and analyzing stock market data. It leverages the Dash framework for the web interface and integrates with various data sources to provide up-to-date information and visualizations.
 
 ### Structure
-```
-├── audio                       <-- Audio Folder
-|   └── recordings              <-- Recordings Folder
-|       └── *.wav               <-- Audio Recordings
-|   └── audioMNIST_meta.txt     <-- Meta Information
-|
-├── config                      <-- Configuration Folder
-|   └── *.yaml                  <-- Configuration Files
-|
-├── data                        <-- Data Folder
-|   └── *.csv                   <-- Data Files
-|
-├── images                      <-- Image Folder
-|   └── *.png                   <-- Image Files
-|
-├── logs                        <-- Log Folder
-|   └── *.log                   <-- Log Files
-|
-├── parameters                  <-- Parameters Folder
-|   └── *.yaml                  <-- Model Parameters
-|
-├── plots                       <-- Plots Folder
-|   └── *.png                   <-- Plots
-|
-├── results                     <-- Results Folder
-|   └── *.yaml                  <-- Model Results
-|
-├── src                         <-- Source Folder
-|   └── *.py                    <-- Source Files
-|
-├── test                        <-- Test Folder
-|   └── *.py                    <-- Unit Tests
-|
-├── text                        <-- Text Folder
-|   └── *.txt                   <-- Text Files
-|
-├── .gitignore                  <-- Git Ignore Configuration
-|
-├── .pre-commit-config.yaml     <-- Pre-Commit Configuration
-|
-├── audio_mnist.py              <-- Main Python Script
-|
-├── audio_mnist.sh              <-- Main Shell Script
-|
-├── flowchart.wsd               <-- Pipeline Flowchart
-|
-├── gui.py                      <-- GUI Python Script
-|
-├── readme.md                   <-- You Are Here
-|
-├── requirements.txt            <-- Package Requirements
-```
 
-### Dataset
-This project utilizes a comprehensive [dataset](https://www.kaggle.com/datasets/primaryobjects/voicegender) encompassing 30,000 spoken digits (0-9) audio samples from 60 distinct speakers. The dataset includes a directory for each speaker containing their respective audio recordings and a meta-information file detailing the gender and age of each speaker.
 
-### Model Selection
-The choice of [XGBoost](https://xgboost.readthedocs.io/en/stable/) (eXtreme Gradient Boosting) is grounded in its efficiency and scalability, especially for large datasets and complex models. It stands out for its capacity to manage missing values, categorical variables, and high-dimensional data, making it aptly suited for the project. Moreover, it encompasses regularization techniques that deter overfitting and enhance model generalization. 
-
-### Model Architecture
-The project employs a gradient boosting tree ensemble model within the XGBoost framework. This model synergizes predictions from several decision trees to arrive at a final prediction for the target variable — the speaker's gender. A set of key parameters govern the model, allowing for fine-tuning to attain optimal performance on the dataset.
-
-The model uses the following key parameters:
-| Parameter       | Description          |
-|-----------------|----------------------|
-| `learning_rate` | Learning Rate        |
-| `max_depth`     | Max Depth Of Tree    |
-| `n_estimators`  | Number Of Estimators |
-| `objective`     | Objective Function   |
-| `tree_method`   | Tree Method          |
-
-### Model Features
-The model relies on various statistical features calculated from both the time-domain and frequency-domain data of the audio samples. These features are crucial in training the model to recognize gender-specific patterns in the voice recordings.
-
-These features include:
-| Feature     | Description        |
-|-------------|--------------------|
-| `mean`      | Mean               |
-| `std`       | Standard Deviation |
-| `med`       | Median             |
-| `min`       | Minimum            |
-| `q25`       | 25th Percentiles   |
-| `q75`       | 75th Percentiles   |
-| `max`       | Maximum            |
-| `skew`      | Skewness           |
-| `kurt`      | Kurtosis           |
-| `zeroxrate` | Zero Crossing Rate |
-| `entropy`   | Entropy            |
-| `sfm`       | Spectral Flatness  |
-| `cent`      | Frequency Centroid |
-
-### Model Performance
-The model has achieved a promising accuracy rate of `96.70%` on the test dataset, showcasing its potential in gender recognition through voice analysis.
-
-### PostgreSQL Integration
-PostgreSQL integration enhances the project's data management capabilities. By utilizing [PostgreSQL](https://www.postgresql.org), an open-source object-relational database system, we ensure scalability, robustness, and efficient storage and retrieval of data. Utilize psycopg2, a Python driver, for seamless interaction with PostgreSQL. Store connection details in the `config/postgres.yaml` file for easy access, and perform database operations using SQL queries.
-
-### Conclusion
-This project epitomizes the application of machine learning in voice and speech analysis, showcasing the potential to discern gender through acoustic features.
+### Features
+- **Real-time Data**: Displays real-time stock data for the S&P 500 index.
+- **Historical Performance**: Visualizes historical stock performance over various time periods.
+- **Investment Prediction**: Provides tools for predicting future investment values.
+- **Clock and Date**: Displays the current time and date on the dashboard.
 
 ### Requirements
 Execute `pip install -r requirements.txt` to install the required libraries.
 
 ### Execution
-Execute `audiomnist.sh` to initiate the entire pipeline.
-
-Following arguments can be specified:
-| Argument              | Description                               |
-|-----------------------|-------------------------------------------|
-| `-c`, `--cfg_file`    | Path to the configuration file            |
-| `-y`, `--pgs_file`    | Path to the PostgreSQL configuration file |
-| `-d`, `--data_prep`   | Execute the data preparation step         |
-| `-f`, `--feat_eng`    | Execute the feature engineering step      |
-| `-s`, `--data_split`  | Execute the data splitting step           |
-| `-u`, `--model_tune`  | Execute the model tuning step             |
-| `-t`, `--model_train` | Execute the model training step           |
-| `-p`, `--model_pred`  | Execute the model prediction step         |
-| `-q`, `--data_sql`    | Execute the data sql step                 |
-
-
-### Unit Tests
-Execute `python -m unittest discover test` to run all unit tests, ensuring the reliability of the code base.
+Execute `python stocks.py` to run the dashboard locally.
 
 ### Developer
 Execute `python -m pre_commit run --all-files` to ensure code quality and formatting checks.
--- stocks.py
+++ stocks.py
@@ -1,20 +1,21 @@
+import warnings
+from datetime import datetime
+
 import dash
-from dash import dcc, html, Input, Output, State
+import dash_daq as daq
 import plotly.graph_objs as go
 import yfinance as yf
-from datetime import datetime
-import dash_daq as daq
-import warnings
+from dash import Input, Output, State, dcc, html
 
 # Suppress specific FutureWarning from yfinance
-warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 app = dash.Dash(__name__)
 
 # Fetch S&P 500 data
-def fetch_data(period='1y'):
+def fetch_data(period="1y"):
     try:
-        df = yf.download('^GSPC', period=period)
+        df = yf.download("^GSPC", period=period)
         if df.empty:
             raise ValueError("No data found for the given period.")
         return df
@@ -22,8 +23,15 @@ def fetch_data(period='1y'):
         print(f"Error fetching data: {e}")
         return None
 
+
 # Investment calculation function
-def calculate_investment(initial_investment, monthly_investment, num_years, annual_interest_rate, ongoing_charges_rate):
+def calculate_investment(
+    initial_investment,
+    monthly_investment,
+    num_years,
+    annual_interest_rate,
+    ongoing_charges_rate,
+):
     low_tax = 0.27
     high_tax = 0.42
     threshold_tax = 61000
@@ -64,147 +72,280 @@ def calculate_investment(initial_investment, monthly_investment, num_years, annu
 
     return money_invested_yearly, investment_yearly, profits_yearly
 
+
 # Layout components
 def create_layout():
-    return html.Div(style={'backgroundColor': '#1e1e1e', 'color': '#ffffff'}, children=[
-        html.H1("Stock Dashboard", style={'textAlign': 'center'}),
-        dcc.Markdown(
-            """
+    return html.Div(
+        style={"backgroundColor": "#1e1e1e", "color": "#ffffff"},
+        children=[
+            html.H1("Stock Dashboard", style={"textAlign": "center"}),
+            dcc.Markdown(
+                """
             This dashboard provides real-time data and visualizations for the S&P 500 index.  
             You can view stock performance over various time periods and predict future investment values.
             """,
-            style={'textAlign': 'center', 'margin': '20px auto', 'maxWidth': '800px'}
-        ),
-        create_clock_and_date(),
-        html.H2("S&P 500 History", style={'textAlign': 'center'}),
-        dcc.Graph(id='stock-graph'),
-        create_period_buttons(),
-        create_investment_prediction_section()
-    ])
+                style={
+                    "textAlign": "center",
+                    "margin": "20px auto",
+                    "maxWidth": "800px",
+                },
+            ),
+            create_clock_and_date(),
+            html.H2("S&P 500 History", style={"textAlign": "center"}),
+            dcc.Graph(id="stock-graph"),
+            create_period_buttons(),
+            create_investment_prediction_section(),
+        ],
+    )
+
 
 def create_clock_and_date():
-    return html.Div([
-        daq.LEDDisplay(
-            id='clock',
-            value=datetime.now().strftime('%H:%M:%S'),
-            color="#00cc96",
-            backgroundColor="#1e1e1e",
-            size=24,
-            style={'textAlign': 'left', 'margin': '10px', 'position': 'absolute', 'top': '30px', 'left': '10px'}
-        ),
-        html.Div(id='date', style={'textAlign': 'right', 'margin': '10px', 'color': '#00cc96', 'fontSize': '30px', 'position': 'absolute', 'top': '30px', 'right': '10px', 'backgroundColor': '#1e1e1e'})
-    ])
+    return html.Div(
+        [
+            daq.LEDDisplay(
+                id="clock",
+                value=datetime.now().strftime("%H:%M:%S"),
+                color="#00cc96",
+                backgroundColor="#1e1e1e",
+                size=24,
+                style={
+                    "textAlign": "left",
+                    "margin": "10px",
+                    "position": "absolute",
+                    "top": "30px",
+                    "left": "10px",
+                },
+            ),
+            html.Div(
+                id="date",
+                style={
+                    "textAlign": "right",
+                    "margin": "10px",
+                    "color": "#00cc96",
+                    "fontSize": "30px",
+                    "position": "absolute",
+                    "top": "30px",
+                    "right": "10px",
+                    "backgroundColor": "#1e1e1e",
+                },
+            ),
+        ]
+    )
+
 
 def create_period_buttons():
-    periods = ['1d', '1wk', '1mo', '3mo', '6mo', '1y', '3y', '5y', 'ytd', 'max']
-    return html.Div([
-        html.Button(period.replace('ytd', 'Year to Date').replace('max', 'Max').replace('mo', ' Month').replace('wk', ' Week').replace('d', ' Day').replace('y', ' Year'), id=period, n_clicks=0, style={'margin': '5px'}) for period in periods
-    ], style={'textAlign': 'center'})
+    periods = ["1d", "1wk", "1mo", "3mo", "6mo", "1y", "3y", "5y", "ytd", "max"]
+    return html.Div(
+        [
+            html.Button(
+                period.replace("ytd", "Year to Date")
+                .replace("max", "Max")
+                .replace("mo", " Month")
+                .replace("wk", " Week")
+                .replace("d", " Day")
+                .replace("y", " Year"),
+                id=period,
+                n_clicks=0,
+                style={"margin": "5px"},
+            )
+            for period in periods
+        ],
+        style={"textAlign": "center"},
+    )
+
 
 def create_investment_prediction_section():
-    return html.Div([
-        html.H2("Investment Prediction"),
-        html.Div([
-            create_input_field("Initial Investment (DKK)", 'initial-investment', 100000),
-            create_input_field("Monthly Investment (DKK)", 'monthly-investment', 5000),
-            create_input_field("Number of Years", 'num-years', 20),
-            create_input_field("Annual Interest Rate (%)", 'annual-interest-rate', 7),
-            create_input_field("Ongoing Charges Rate (%)", 'ongoing-charges-rate', 0.07),
-        ], style={'textAlign': 'center'}),
-        html.Button('Predict', id='predict-button', n_clicks=0, style={'margin': '20px'}),
-        dcc.Graph(id='prediction-graph')
-    ], style={'textAlign': 'center', 'margin': '20px auto'})
+    return html.Div(
+        [
+            html.H2("Investment Prediction"),
+            html.Div(
+                [
+                    create_input_field(
+                        "Initial Investment (DKK)", "initial-investment", 100000
+                    ),
+                    create_input_field(
+                        "Monthly Investment (DKK)", "monthly-investment", 5000
+                    ),
+                    create_input_field("Number of Years", "num-years", 20),
+                    create_input_field(
+                        "Annual Interest Rate (%)", "annual-interest-rate", 7
+                    ),
+                    create_input_field(
+                        "Ongoing Charges Rate (%)", "ongoing-charges-rate", 0.07
+                    ),
+                ],
+                style={"textAlign": "center"},
+            ),
+            html.Button(
+                "Predict", id="predict-button", n_clicks=0, style={"margin": "20px"}
+            ),
+            dcc.Graph(id="prediction-graph"),
+        ],
+        style={"textAlign": "center", "margin": "20px auto"},
+    )
+
 
 def create_input_field(label, id, value):
-    return html.Div([
-        html.Label(label),
-        dcc.Input(id=id, type='number', value=value, style={'margin': '10px', 'width': '80px'})
-    ], style={'display': 'inline-block', 'margin': '10px'})
+    return html.Div(
+        [
+            html.Label(label),
+            dcc.Input(
+                id=id,
+                type="number",
+                value=value,
+                style={"margin": "10px", "width": "80px"},
+            ),
+        ],
+        style={"display": "inline-block", "margin": "10px"},
+    )
+
 
 # Callbacks
 @app.callback(
-    Output('stock-graph', 'figure'),
-    [Input(period, 'n_clicks') for period in ['1d', '1wk', '1mo', '3mo', '6mo', '1y', '3y', '5y', 'ytd', 'max']],
-    [State(period, 'id') for period in ['1d', '1wk', '1mo', '3mo', '6mo', '1y', '3y', '5y', 'ytd', 'max']]
+    Output("stock-graph", "figure"),
+    [
+        Input(period, "n_clicks")
+        for period in ["1d", "1wk", "1mo", "3mo", "6mo", "1y", "3y", "5y", "ytd", "max"]
+    ],
+    [
+        State(period, "id")
+        for period in ["1d", "1wk", "1mo", "3mo", "6mo", "1y", "3y", "5y", "ytd", "max"]
+    ],
 )
 def update_graph(*args):
     ctx = dash.callback_context
     if not ctx.triggered:
-        button_id = 'ytd'
+        button_id = "ytd"
     else:
-        button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-    
+        button_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
     period = button_id
     df = fetch_data(period=period)
     if df is None:
         return go.Figure(
             layout=go.Layout(
                 title="No data available for the selected period",
-                template='plotly_dark'
+                template="plotly_dark",
             )
         )
-    
+
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df.index, y=df['Close'], mode='lines', name='S&P 500', line=dict(color='white')))
-    
+    fig.add_trace(
+        go.Scatter(
+            x=df.index,
+            y=df["Close"],
+            mode="lines",
+            name="S&P 500",
+            line=dict(color="white"),
+        )
+    )
+
     # Calculate percentage change
-    start_price = df['Close'].iloc[0]
-    end_price = df['Close'].iloc[-1]
+    start_price = df["Close"].iloc[0]
+    end_price = df["Close"].iloc[-1]
     percentage_change = ((end_price - start_price) / start_price) * 100
     sign = "+" if percentage_change > 0 else ""
     percentage_change_text = f"Change: {sign}{percentage_change:.2f}%"
     current_price_text = f"Current Price: {end_price:.2f} DKK"
-    
+
     fig.update_layout(
-        template='plotly_dark',
-        xaxis_title='Date',
-        yaxis_title='Price (DKK)',
-        title=f"S&P 500 ({period.upper()}) - {percentage_change_text}, {current_price_text}"
+        template="plotly_dark",
+        xaxis_title="Date",
+        yaxis_title="Price (DKK)",
+        title=f"S&P 500 ({period.upper()}) - {percentage_change_text}, {current_price_text}",
     )
-    
+
     return fig
 
-@app.callback(
-    Output('clock', 'value'),
-    Input('interval-component', 'n_intervals')
-)
+
+@app.callback(Output("clock", "value"), Input("interval-component", "n_intervals"))
 def update_clock(n):
-    return datetime.now().strftime('%H:%M:%S')
+    return datetime.now().strftime("%H:%M:%S")
 
-@app.callback(
-    Output('date', 'children'),
-    Input('interval-component', 'n_intervals')
-)
+
+@app.callback(Output("date", "children"), Input("interval-component", "n_intervals"))
 def update_date(n):
-    return datetime.now().strftime('%d-%m-%Y')
+    return datetime.now().strftime("%d-%m-%Y")
+
 
 @app.callback(
-    Output('prediction-graph', 'figure'),
-    Input('predict-button', 'n_clicks'),
-    [State('initial-investment', 'value'), State('monthly-investment', 'value'), State('num-years', 'value'),
-     State('annual-interest-rate', 'value'), State('ongoing-charges-rate', 'value')]
+    Output("prediction-graph", "figure"),
+    Input("predict-button", "n_clicks"),
+    [
+        State("initial-investment", "value"),
+        State("monthly-investment", "value"),
+        State("num-years", "value"),
+        State("annual-interest-rate", "value"),
+        State("ongoing-charges-rate", "value"),
+    ],
 )
-def predict_investment(n_clicks, initial_investment, monthly_investment, num_years, annual_interest_rate, ongoing_charges_rate):
-    if not initial_investment or not monthly_investment or not num_years or not annual_interest_rate or not ongoing_charges_rate:
+def predict_investment(
+    n_clicks,
+    initial_investment,
+    monthly_investment,
+    num_years,
+    annual_interest_rate,
+    ongoing_charges_rate,
+):
+    if (
+        not initial_investment
+        or not monthly_investment
+        or not num_years
+        or not annual_interest_rate
+        or not ongoing_charges_rate
+    ):
         return go.Figure()
-    
+
     annual_interest_rate /= 100
     ongoing_charges_rate /= 100
 
     money_invested_yearly, investment_yearly, profits_yearly = calculate_investment(
-        initial_investment, monthly_investment, num_years, annual_interest_rate, ongoing_charges_rate
+        initial_investment,
+        monthly_investment,
+        num_years,
+        annual_interest_rate,
+        ongoing_charges_rate,
     )
 
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=list(range(num_years + 1)), y=money_invested_yearly, mode='lines', name='Money Invested (DKK)', line=dict(color='blue')))
-    fig.add_trace(go.Scatter(x=list(range(num_years + 1)), y=investment_yearly, mode='lines', name='Investment Value (DKK)', line=dict(color='green')))
-    fig.add_trace(go.Scatter(x=list(range(num_years + 1)), y=profits_yearly, mode='lines', name='Profit (DKK)', line=dict(color='orange')))
-    fig.update_layout(template='plotly_dark', xaxis_title='Years', yaxis_title='Amount (DKK)')
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(num_years + 1)),
+            y=money_invested_yearly,
+            mode="lines",
+            name="Money Invested (DKK)",
+            line=dict(color="blue"),
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(num_years + 1)),
+            y=investment_yearly,
+            mode="lines",
+            name="Investment Value (DKK)",
+            line=dict(color="green"),
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=list(range(num_years + 1)),
+            y=profits_yearly,
+            mode="lines",
+            name="Profit (DKK)",
+            line=dict(color="orange"),
+        )
+    )
+    fig.update_layout(
+        template="plotly_dark", xaxis_title="Years", yaxis_title="Amount (DKK)"
+    )
     return fig
+
 
 # Update clock and date every second
 app.layout = create_layout()
-app.layout.children.append(dcc.Interval(id='interval-component', interval=1*1000, n_intervals=0))
+app.layout.children.append(
+    dcc.Interval(id="interval-component", interval=1 * 1000, n_intervals=0)
+)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
